d8a31637b86346887e3a5740f6fe1bb49d75c06d
Guillaume Mazoyer <guillaume@opsmill.com>
2024-11-06 22:04:12 +0100

    Trigger messages on repo add/sync

--- backend/infrahub/message_bus/operations/git/repository.py
+++ backend/infrahub/message_bus/operations/git/repository.py
@@ -4,13 +4,14 @@ from infrahub import lock
 from infrahub.core.constants import RepositoryInternalStatus
 from infrahub.exceptions import RepositoryError
 from infrahub.git.repository import InfrahubReadOnlyRepository, InfrahubRepository, get_initialized_repo
-from infrahub.log import get_logger
-from infrahub.message_bus import messages
+from infrahub.log import get_log_data, get_logger
+from infrahub.message_bus import Meta, messages
 from infrahub.message_bus.messages.git_repository_connectivity import (
     GitRepositoryConnectivityResponse,
     GitRepositoryConnectivityResponseData,
 )
 from infrahub.services import InfrahubServices
+from infrahub.worker import WORKER_IDENTITY
 
 log = get_logger()
 
@@ -38,6 +39,17 @@ async def add_read_only(message: messages.GitRepositoryAddReadOnly, service: Inf
             await repo.import_objects_from_files(infrahub_branch_name=message.infrahub_branch_name)
             if message.internal_status == RepositoryInternalStatus.ACTIVE.value:
                 await repo.sync_from_remote()
+
+                # Notify other workers they need to clone the repository
+                notification = messages.GitRepositoryClone(
+                    meta=Meta(initiator_id=WORKER_IDENTITY, request_id=get_log_data().get("request_id", "")),
+                    location=message.location,
+                    repository_id=message.repository_id,
+                    repository_name=message.repository_name,
+                    default_branch_name=message.default_branch_name,
+                    infrahub_branch_name=message.infrahub_branch_name,
+                )
+                await service.send(message=notification)
 
 
 @flow(name="git-repository-check-connectivity")
